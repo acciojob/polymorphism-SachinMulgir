1470e868e98f226039a0bc7ca8e7159489a4c438
SachinMulgir <smulgir636@gmail.com>
2023-04-27 00:35:46 +0530

    made the product class public

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,9 @@
 package com.driver;
 
 public class Main {
-    static class Product{
+    public static class Product {
         public int product(int x, int y){
-            return x * y;
+            return x*y;
         }
         public int product( int x, int y, int z){
             return (x*y*z);
@@ -16,9 +16,9 @@ public class Main {
 
     public static void main(String[] args) {
         Product p = new Product();
-        p.product(1,2);
-        p.product(1,2,3);
-        p.product(1.2, 2.3);
+        System.out.println(p.product(1,2));
+        System.out.println(p.product(1,2,3));
+        System.out.println(p.product(1.2, 2.3));
     }
 
 }
